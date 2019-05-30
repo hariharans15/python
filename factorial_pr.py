34a96d45a4c0b5d1a5e80b8b7d418166768737d7
hariharans15 <51091430+hariharans15@users.noreply.github.com>
2019-05-30 20:35:42 +0530

    Update factorial_pr.py

--- factorial_pr.py
+++ factorial_pr.py
@@ -1,8 +1,5 @@
-def fact(n):
-    if n==1 or n==0:
-        return 1
-    else:
-        return n*fact(n-1)
-
-f=int(input())
-print(fact(f))
+r=int(input())
+fact=1
+for i in range(1,r+1):
+   fact=fact*i
+print(fact)
