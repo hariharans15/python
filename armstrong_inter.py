a1c1b48889c061cd8fb3d613dca2a6933faf15fc
hariharans15 <51091430+hariharans15@users.noreply.github.com>
2019-05-30 12:01:47 +0530

    Create armstrong_inter.py

---
+++ armstrong_inter.py
@@ -0,0 +1,13 @@
+s,r=map(int,input().split())
+a=str(s)
+l=len(a)
+#print(l)
+for i in range(s,r+1):
+    tema=i
+    rem=0
+    while(i!=0):
+        dig=i%10
+        rem=rem+dig**l
+        i=i//10
+    if(tema==rem):
+        print(rem,end=" ")
